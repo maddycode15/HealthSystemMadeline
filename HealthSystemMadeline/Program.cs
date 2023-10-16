b9d411a5a37e363561476885a963c0d15b419d72
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-10-16 10:42:01 -0300

    day 2

--- HealthSystemMadeline/Program.cs
+++ HealthSystemMadeline/Program.cs
@@ -1,2 +1,48 @@
 ﻿//madeline health systems project
 
+// establishing variables 
+
+int health;
+
+int lives;
+
+int sheild;
+
+string healthStatus;
+
+
+
+// establishing values
+
+health = 100;
+
+lives = 3;
+
+sheild = 3;
+
+
+
+
+
+
+
+// show hud
+
+if (health = 100  //healthStatus = "perfect health");
+
+Console.Write("sheild =" + " " + sheild);
+
+Console.WriteLine(" ");
+
+Console.WriteLine("health =" + " " + health);
+
+Console.WriteLine(healthStatus);
+
+Console.WriteLine("lives =" + " " + lives);
+
+//show health status
+
+
+
+//take damage
+
